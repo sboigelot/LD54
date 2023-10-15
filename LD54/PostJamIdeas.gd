e4e0aaa284952820f9041d5e563cd61e903e15b3
Simon Boigelot <sboigelot@gmail.com>
2023-10-15 15:26:27 +0200

    Post Jam ideas

---
+++ LD54/PostJamIdeas.gd
@@ -0,0 +1,53 @@
+""" I DO - LD54 
+
+Priority 1:
+ 
+	Bugs:
+		
+		Dropping invitation outside the wedding area --> should go back to guest list
+		When mousing over traits they stay behind the guest pics 
+		correct typos 
+		Tooltip test goes behind polaroid avatar
+
+	Features:
+
+		Add toggle to play Post Jam / Jam version
+		Add change log
+		 
+		I do - single: description could be "ready to mingle" 
+		Fix emoji - make it round or more cube 
+		Change Karene and Sandy look (they look the same)
+		Change Live Log to wedding and text should something like 
+			“I was happy to talk about Space with Eloise. (signed : Filip)"
+		Sound - page turning sound when switching page like a book (Finding better sounds)
+			Adding sound of speaking (angry / happy)
+			Cafetaria sound when eating / drinking
+
+	??? Fix colors for traits 
+
+Priority 2:
+	
+	Features:
+
+		Little animations of uncle bob making a mess?? (he vomits and people have shocked angry face emoji 
+		Smaller wedding tutorial
+		Making the character move a bit on their chair when they speak
+		
+		Carrying a glass of champaign
+		Carrying a plate
+		
+		
+----------------------------------------------------------------------------------------------------
+
+SOme funny limited space ideas 
+
+Just one pan and some jiggle physics - jigly egg, veggies. Drag things to squish all the ingredients. 
+	Making an omelette or something 
+
+Squeeze with your hubby in a small bed. One again silly physics mechanic. 
+	he changes pose every x time or something and you need to fit into bed around him. 
+
+
+
+
+"""
